7958897e042df102a43fd142005cf84e804a2cde
Ken Kamata <kn12kamata@gmail.com>
2023-08-27 07:58:49 +0900

    chore: Rename file name

--- rondo/utils/__init__.py
+++ rondo/utils/__init__.py
@@ -2,4 +2,4 @@ from rondo.utils.array import as_array
 from rondo.utils.array import as_vec
 from rondo.utils.array import as_mat
 
-from rondo.utils.numeric import numerical_diff
+from rondo.utils.numerical import numerical_diff
