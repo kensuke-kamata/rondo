f8815d833648ebb33eb8be61691f84349f11c58c
Ken Kamata <kn12kamata@gmail.com>
2023-08-26 14:43:10 +0900

    feat: Support variable length inputs in numerical_diff

--- rondo/utils/numeric.py
+++ rondo/utils/numeric.py
@@ -2,9 +2,18 @@ import rondo
 from rondo.variable import Variable
 from rondo.utils import as_array
 
-def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(as_array(x.data - eps))
-    x1 = Variable(as_array(x.data + eps))
-    y0 = f(x0)
-    y1 = f(x1)
-    return (y1.data - y0.data) / (2 * eps)
+def numerical_diff(f, *inputs, eps=1e-4):
+    grads = []
+    inputs = list(inputs)
+    for i, input in enumerate(inputs):
+        tmp0 = inputs.copy()
+        tmp1 = inputs.copy()
+        x0 = Variable(as_array(input.data - eps))
+        x1 = Variable(as_array(input.data + eps))
+        tmp0[i] = x0
+        tmp1[i] = x1
+        y0 = f(*tmp0)
+        y1 = f(*tmp1)
+        grad = (y1.data - y0.data) / (2 * eps)
+        grads.append(grad)
+    return grads if len(grads) > 1 else grads[0]
