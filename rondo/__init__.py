007057b947e6386f6a9ed7f1a829c318eeb5bdf8
Ken Kamata <kn12kamata@gmail.com>
2023-09-03 08:53:41 +0900

    feat: Add Layer class

--- rondo/__init__.py
+++ rondo/__init__.py
@@ -1,11 +1,13 @@
+from rondo.config import Config
+from rondo.config import using
+from rondo.config import no_grad
+
+from rondo.layer import Layer
+
 from rondo.variable import Variable
 from rondo.variable import Parameter
 from rondo.variable import as_array
 from rondo.variable import as_variable
-
-from rondo.config import Config
-from rondo.config import using
-from rondo.config import no_grad
 
 from rondo import functions
 from rondo import utils
