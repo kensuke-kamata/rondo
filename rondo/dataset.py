733569c000ba8c60dcdf1de4cf6df84807d47cb8
Ken Kamata <kn12kamata@gmail.com>
2023-09-07 07:33:44 +0900

    refactor: Rename member variables

--- rondo/dataset.py
+++ rondo/dataset.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 class Dataset:
-    def __init__(self, train=True, transform=None, target_transform=None) -> None:
+    def __init__(self, train=True, transform_x=None, transform_t=None) -> None:
         self.train = train
-        self.transform = transform
-        self.target_transform = target_transform
-        if self.transform is None:
-            self.transform = lambda x: x
-        if self.target_transform is None:
-            self.target_transform = lambda x: x
+        self.transform_x = transform_x
+        self.transform_t = transform_t
+        if self.transform_x is None:
+            self.transform_x = lambda x: x
+        if self.transform_t is None:
+            self.transform_t = lambda x: x
 
         self.data = None
         self.label = None
@@ -17,9 +17,9 @@ class Dataset:
     def __getitem__(self, index):
         assert np.isscalar(index)
         if self.label is None:
-            return self.transform(self.data[index]), None
+            return self.transform_x(self.data[index]), None
         else:
-            return self.transform(self.data[index]), self.target_transform(self.label[index])
+            return self.transform_x(self.data[index]), self.transform_t(self.label[index])
 
     def __len__(self):
         return len(self.data)
