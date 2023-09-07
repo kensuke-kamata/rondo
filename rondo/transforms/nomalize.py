da9e6799ab5593b276109c61fc82be9c29331e3f
Ken Kamata <kn12kamata@gmail.com>
2023-09-08 07:53:00 +0900

    chore: Rename variables

--- rondo/transforms/nomalize.py
+++ rondo/transforms/nomalize.py
@@ -5,15 +5,15 @@ class Normalize:
         self.mean = mean
         self.std = std
 
-    def __call__(self, sample):
+    def __call__(self, array):
         mean, std = self.mean, self.std
 
         if not np.isscalar(mean):
-            mshape = [1] * sample.ndim
-            mshape[0] = len(sample) if len(mean) == 1 else len(mean)
-            mean = np.array(mean, dtype=sample.dtype).reshape(*mshape)
+            mshape = [1] * array.ndim
+            mshape[0] = len(array) if len(mean) == 1 else len(mean)
+            mean = np.array(mean, dtype=array.dtype).reshape(*mshape)
         if not np.isscalar(std):
-            rshape = [1] * sample.ndim
-            rshape[0] = len(sample) if len(std) == 1 else len(std)
-            std = np.array(std, dtype=sample.dtype).reshape(*rshape)
-        return (sample - mean) / std
+            rshape = [1] * array.ndim
+            rshape[0] = len(array) if len(std) == 1 else len(std)
+            std = np.array(std, dtype=array.dtype).reshape(*rshape)
+        return (array - mean) / std
