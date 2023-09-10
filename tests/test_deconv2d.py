c646c6644756e791380fa707850d4c9712bcd07a
Ken Kamata <kn12kamata@gmail.com>
2023-09-11 08:40:40 +0900

    chore: Change test parameters and add console output

--- tests/test_deconv2d.py
+++ tests/test_deconv2d.py
@@ -6,10 +6,10 @@ class TestDeconv2d(unittest.TestCase):
     def test_forward(self):
         # The number of channels has to be the same between x and W
         # (N, C, IH, IW)
-        x = np.random.rand(1, 3, 4, 4) # Batch size of 1, 3 channels, 4x4 image
+        x = np.random.rand(1, 2, 3, 3) # Batch size of 1, 2 channels, 3x3 filter
         # (C, OC, KH, KW)
-        W = np.random.rand(3, 2, 3, 3) # 3 input channels, 2 output channels, 3x3 filter
-        b = np.random.rand(2)
+        W = np.random.rand(2, 3, 4, 4) # 2 input channels, 3 output channels (RGB), 4x4 image
+        b = np.random.rand(3)
         stride = 1
         pad = 1
 
@@ -24,3 +24,7 @@ class TestDeconv2d(unittest.TestCase):
         # Call the forward method
         y = rondo.functions.deconv2d(x, W, b, stride, pad)
         self.assertEqual(y.data.shape, expected_shape)
+
+        # Console output
+        print(x)
+        print(y.data)
