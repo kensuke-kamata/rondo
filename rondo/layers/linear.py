a54c4c0149a8a9977e93b85a310866929ab667bb
Ken Kamata <kn12kamata@gmail.com>
2023-09-12 08:37:54 +0900

    chore: Rename parameters

--- rondo/layers/linear.py
+++ rondo/layers/linear.py
@@ -4,29 +4,29 @@ import rondo
 import rondo.functions as F
 
 class Linear(rondo.Layer):
-    def __init__(self, out_size, in_size=None, nobias=False, dtype=np.float32):
+    def __init__(self, size_out, size_in=None, nobias=False, dtype=np.float32):
         super().__init__()
-        self.in_size = in_size
-        self.out_size = out_size
+        self.size_in = size_in
+        self.size_out = size_out
         self.dtype = dtype
 
         self.W = rondo.Parameter(None, name='W')
-        if self.in_size is not None:
+        if self.size_in is not None:
             self._init_W()
 
         if nobias:
             self.b = None
         else:
-            self.b = rondo.Parameter(np.zeros(out_size, dtype=dtype), name='b')
+            self.b = rondo.Parameter(np.zeros(size_out, dtype=dtype), name='b')
 
     def _init_W(self):
-        I, O = self.in_size, self.out_size
+        I, O = self.size_in, self.size_out
         W_data = np.random.randn(I, O).astype(self.dtype) * np.sqrt(1 / I)
         self.W.data = W_data
 
     def forward(self, x):
         if self.W.data is None:
-            self.in_size = x.shape[1]
+            self.size_in = x.shape[1]
             self._init_W()
 
         y = F.linear(x, self.W, self.b)
